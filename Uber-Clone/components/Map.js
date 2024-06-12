60978382ccd3a082d968887a449cc065ce321e34
SDW-SWD <wickramsuriyashehan@gmail.com>
2024-06-12 17:32:40 +0530

    bi]ug fix in user effect !!

--- Uber-Clone/components/Map.js
+++ Uber-Clone/components/Map.js
@@ -15,9 +15,9 @@ const Map = () => {
   useEffect(()=>{
     if(!orgin || !destination) return;
     mapRef.current.fitToSuppliedMarkers(['origin','destination'],{
-        edgePadding:{top:50,right:50,bottom:50,left:50}
+        edgePadding:{ top:50,right:50,bottom:50,left:50 }
     })
-  },[])
+  },[orgin,destination])
 
   return (
     <MapView
